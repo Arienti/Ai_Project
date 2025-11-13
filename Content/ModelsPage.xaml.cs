6a410f3dbeeed7c8a33ea7b68d71cbc76517df6e
arientllupi@gmail.com <arientllupi@gmail.com>
2025-11-13 20:04:13 +0100

    test downloading huggingface

--- Content/ModelsPage.xaml.cs
+++ Content/ModelsPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using Ai_Project.DTO;
+using Ai_Project.Services;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Ai_Project.Content
 {
@@ -7,12 +12,89 @@ namespace Ai_Project.Content
     /// </summary>
     public partial class ModelsPage : Page, Utility.InitializablePage
     {
+        List<ModelDTO> models;
+        HuggingFaceService HuggingFaceService;
         public ModelsPage()
         {
+            models = new List<ModelDTO>();
+            HuggingFaceService = new HuggingFaceService();
             InitializeComponent();
         }
         public void Init()
         {
+            LoadModels().GetAwaiter();
+        }
+        private async Task LoadModels()
+        {
+            // Implementation for loading models goes here
+            ResultDTO result = await HuggingFaceService.GetModelsAsync();
+            if (result.IsSuccess && result.Data is List<ModelDTO> fetchedModels)
+            {
+                models = fetchedModels;
+                ModelsListBox.ItemsSource = models;
+            }
+            ModelsCountTextblock.Text = $"Models {models.Count}";
+        }
+
+        private async void ModelsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ModelsListBox.SelectedItem == null)
+                return;
+
+            // If your items are strings (like "maya-research/maya1")
+            ModelDTO? m = ModelsListBox.SelectedItem as ModelDTO;
+            if (m == null)
+                return;
+            // OR if your items are objects:
+            // var model = ModelsListBox.SelectedItem as ModelDTO;
+            // string modelId = model?.Id;
+
+            try
+            {
+                ResultDTO result = await HuggingFaceService.GetModelInfo(m.modelId);
+                if (result.IsSuccess)
+                {
+                    ModelInfoDTO? model = result.Data as ModelInfoDTO;
+                    if (model == null)
+                        return;
+
+                    ModelInfoTextBlock.Text = $"Created: {model.createdAt.ToString("G")}";
+                    ModelInfoSizeTextBlock.Text = $"Size: {model.usedStorage.ToString()}Gb";
+                }
+                else
+                {
+                    MessageBox.Show($"Failed to get model info: {result.ErrorMessages}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error fetching model info: {ex.Message}");
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ModelDTO? m = ModelsListBox.SelectedItem as ModelDTO;
+            if (m == null)
+                return;
+            ResultDTO result = await HuggingFaceService.GetModelInfo(m.modelId);
+            if (result.IsSuccess)
+            {
+                ModelInfoDTO? model = result.Data as ModelInfoDTO;
+                if (model == null)
+                    return;
+                // Application root
+                string appRoot = AppContext.BaseDirectory;
+
+                // Create "Downloads" folder under root
+                string downloadsFolder = Path.Combine(appRoot, "Downloads");
+
+                await HuggingFaceService.DownloadModelFilesAsync(model.id, appRoot);
+            }
+            else
+            {
+                MessageBox.Show($"Failed to get model info: {result.ErrorMessages}");
+            }
         }
     }
 }
---
+++ DTO/ModelDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ai_Project.DTO
+{
+    public class ModelDTO
+    {
+        public string modelId { get; set; }
+        public string pipeline_tag { get; set; }
+        public string createdAt { get; set; }
+        public string[] tags { get; set; }
+        public string CardData { get; set; }
+        public string license { get;set; }
+    }
+}
